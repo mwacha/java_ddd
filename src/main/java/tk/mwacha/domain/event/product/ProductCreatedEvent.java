c2cbcd40d80131149a29db5cb9e56586e6081f21
mwacha <mwacha@gmail.com>
2022-09-18 12:15:43 -0300

    add new test

--- src/main/java/tk/mwacha/domain/event/product/ProductCreatedEvent.java
+++ src/main/java/tk/mwacha/domain/event/product/ProductCreatedEvent.java
@@ -1,16 +1,17 @@
 package tk.mwacha.domain.event.product;
 
+import lombok.Getter;
 import tk.mwacha.domain.event.shared.EventInterface;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+
 
 public class ProductCreatedEvent implements EventInterface {
     private LocalDateTime dateTimeOccurred;
-   // T eventData = null;
+    private Object eventData;
 
-    public<T> ProductCreatedEvent(T event){
+    public ProductCreatedEvent(Object event){
         dateTimeOccurred = LocalDateTime.now();;
-      //  eventData = event;
+        eventData = event;
     }
 }
--- src/main/java/tk/mwacha/domain/event/shared/EventInterface.java
+++ src/main/java/tk/mwacha/domain/event/shared/EventInterface.java
@@ -1,8 +1,5 @@
 package tk.mwacha.domain.event.shared;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 public interface EventInterface {
 
 }
